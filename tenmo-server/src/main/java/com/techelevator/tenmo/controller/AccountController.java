8b84d0ca2138fa686e451c6ec7ae7769ac77d961
Bob Gongaware <robertgongaware@gmail.com>
2020-11-06 10:04:42 -0500

    Updates to controller

--- tenmo-server/src/main/java/com/techelevator/tenmo/controller/AccountController.java
+++ tenmo-server/src/main/java/com/techelevator/tenmo/controller/AccountController.java
@@ -4,6 +4,7 @@ import java.security.Principal;
 
 import javax.validation.Valid;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -35,10 +36,7 @@ public class AccountController {
 	public Account viewCurrentBalance(@PathVariable("id") Long accountId) {
 		return accountDAO.viewCurrentBalance(accountId);
 }
-	@RequestMapping(path = "", method = RequestMethod.PUT)
-	public boolean updateReceiverAccountBalance(@Valid @RequestBody Long accountId) {
-		return true;
-	}
+	
 //	@PreAuthorize("hasRole('USER')")
 //	@RequestMapping(path = "/{id}", method = RequestMethod.PUT)
 //	public boolean updateSenderAccountBalance(@Valid @RequestBody Long accountId) {
